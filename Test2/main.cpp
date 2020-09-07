4b465771ca7c9a265e8b61de9cded602b4fcf678
WangYMei <2385405667@qq.com>
2020-09-07 23:50:52 +0800

    Initial Commit

---
+++ Test2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Test2
+//
+//  Created by Amy Wang on 2020/9/7.
+//  Copyright © 2020 Amy Wang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
