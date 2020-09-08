89a600ffdc5d57d7e2d375c714d8ab1e0abc0479
WangYMei <2385405667@qq.com>
2020-09-08 08:22:56 +0800

    Use namespace

--- Test2/main.cpp
+++ Test2/main.cpp
@@ -7,9 +7,10 @@
 //
 
 #include <iostream>
+using namespace std;
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";//test
+    cout << "Hello, World!\n";//test
     return 0;
 }
